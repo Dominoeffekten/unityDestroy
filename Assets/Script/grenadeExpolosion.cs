a91d744ce72d096954b78efda05de481a964b17b
Dominoeffekten <rikkedomino@gmail.com>
2020-08-28 08:44:10 +0200

    Bombe

---
+++ Assets/Script/grenadeExpolosion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grenadeExpolosion : MonoBehaviour {
+    //timer for explosion
+    float timer = 2;
+    float countdown;
+    //default false
+    bool hasExploded;
+
+    //GameObject
+    public GameObject explosionParticle;
+
+    // Start is called before the first frame update
+    void Start() {
+        countdown = timer;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //lowering the countdown
+        countdown -= Time.deltaTime;
+
+        
+        if(countdown <= 0 && !hasExploded){
+            Explode();
+        }
+    }
+    //Make it explode 
+    void Explode() {
+        Instantiate(explosionParticle, transform.position, transform.rotation);
+        hasExploded = true;
+    }
+}
