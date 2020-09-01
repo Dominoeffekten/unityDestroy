07770aa87f6890b1cc0388f7bca5502d506ed385
FU ELM <rikkedomino@gmail.com>
2020-09-01 14:04:12 +0200

    Ødelæggelse af elementer

---
+++ Assets/Script/BrokenItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenItem : MonoBehaviour
+{
+    //makes a compoment 
+    public GameObject replacement;
+
+    void OnCollisionEnter(){
+        //an extising object that you wan to make a copy of
+        GameObject.Instantiate(replacement, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
